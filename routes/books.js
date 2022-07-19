9205cc586106d4de2cf843d98cfc0419c7d19bfc
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-07-19 15:18:55 -0300

    feat: books basic CRUD with MySQL

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 
-const connection = require("../database/connection");
+const pool = require("../database/pool");
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
     const { title, pageqty } = req.body;
 
     if(!title || !pageqty){
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
 
     const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
 
-    connection.query(sql, (err, data) => {
+    pool.query(sql, (err, data) => {
         if(err){
             console.log(err);
             res.status(400).json({ error: "Ocorreu um erro ao tentar cadastrar no banco."});
@@ -23,12 +23,10 @@ router.post('/', async (req, res) => {
     })
 })
 
-router.get('/:title', async(req, res) => {
-    const { title } = req.params;
-    const sql = `SELECT * FROM books 
-    WHERE title LIKE '${title}%'`
+router.get('/', (req, res) => {
+    const sql = `SELECT * FROM books`
 
-    connection.query(sql, (err, data) => {
+    pool.query(sql, (err, data) => {
         if(err){
             console.log(err);
             res.status(400).json({ error: "Ocorreu ao tentar recuperar dados do banco."});
@@ -38,5 +36,83 @@ router.get('/:title', async(req, res) => {
         } 
     })
 })
+
+/* Get by id */
+router.get('/book/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = `SELECT * FROM books 
+    WHERE id = '${id}%'`
+
+    pool.query(sql, (err, data) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({ error: "Ocorreu ao tentar recuperar dados do banco."});
+            return;
+        } else {
+            res.status(200).json({ data: data })
+        } 
+    })
+})
+
+module.exports = router;
+
+/* Search by title */
+router.get('/search/:title', (req, res) => {
+    const { title } = req.params;
+    const sql = `SELECT * FROM books 
+    WHERE title LIKE '${title}%'`
+    
+
+    pool.query(sql, (err, data) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({ error: "Ocorreu ao tentar recuperar dados do banco."});
+            return;
+        } else {
+            res.status(200).json({ data: data })
+        } 
+    })
+})
+
+/* Update */
+router.put('/book/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, pageqty } = req.body;   
+
+    if(!title || !pageqty){
+        res.status(400).json({ error: "Está faltando um parâmetro no body"});
+        return;
+    }
+
+    const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = '${id}'`
+
+    pool.query(sql, (err, data) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({ error: "Ocorreu ao tentar atualizar os dados no banco."});
+            return;
+        } else {
+            res.status(200).json({ data: data })
+        } 
+    })
+})
+
+/* Delete */
+router.delete('/book/:id', (req, res) => {
+    const { id } = req.params;
+
+    const sql = `DELETE FROM books WHERE id = '${id}'`
+
+    pool.query(sql, (err, data) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({ error: "Ocorreu ao tentar remover os itens do banco."});
+            return;
+        } else {
+            res.status(200).json({ data: data })
+        } 
+    })
+})
+
 
 module.exports = router;
