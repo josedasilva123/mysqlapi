72f40fc70524a48cb098771867dfc41efac989c7
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-07-20 10:46:56 -0300

    feat: register and login route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 const BookRoutes = require("./routes/books");
+const UserRoutes = require("./routes/user");
 
 app.use(
   express.urlencoded({
@@ -14,6 +15,7 @@ app.use(
 app.use(express.json());
 
 app.use("/books", BookRoutes);
+app.use("/user", UserRoutes);
 
 app.listen(port, () => {
   console.log("API inciada na porta: " + port);
---
+++ routes/user.js
@@ -0,0 +1,102 @@
+const router = require("express").Router();
+
+const pool = require("../database/pool");
+
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({ error: "Está faltando um parâmetro no body" });
+    return;
+  }
+
+  const sql = `SELECT * FROM users WHERE ?? = ?`;
+  const data = ["email", email];
+
+  pool.query(sql, data, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: "Ocorreu um erro ao tentar registrar dados no banco." });
+      return;
+    } else {
+      if (data?.length === 0) {
+        const hashPassword = bcrypt.hashSync(password, 1);
+
+        const currentDate = new Date();
+
+        const registerSql = `INSERT INTO users (??, ??, ??, ??, ??) VALUES (?,?,?,?,?)`;
+
+        const registerData = [
+          "name",
+          "email",
+          "password",
+          "createdAt",
+          "updatedAt",
+          name,
+          email,
+          hashPassword,
+          currentDate,
+          currentDate,
+        ];
+
+        pool.query(registerSql, registerData, (err, data) => {
+          if (err) {
+            console.log(err);
+            res.status(400).json({ error: "Ocorreu um erro ao tentar cadastrar no banco." });
+            return;
+          } else {
+            res.status(200).json({ message: "Usuário cadastrado com sucesso!", data: data });
+          }
+        });
+      } else {
+        res.status(400).json({ error: "O e-mail fornecido já está associoado a outro usuário." });
+        return;
+      }
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ error: "Está faltando um parâmetro no body" });
+    return;
+  }
+
+  const sql = `SELECT * FROM users WHERE ?? = ?`;
+  const data = ["email", email];
+
+  pool.query(sql, data, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ error: "Ocorreu um erro ao tentar cadastrar no banco." });
+      return;
+    } else {
+      if(data[0]){
+        const verifyPassword = bcrypt.compareSync(password, data[0].password);
+
+        if (!verifyPassword) {
+            res.status(400).json({ error: "Sua senha está incorreta." });
+            return;
+        }
+
+        const userData = {
+            name: data[0].name,
+            email: data[0].email,
+            createdAt: data[0].createdAt,
+            updatedAt: data[0].updatedAt,
+        }
+
+        res.status(200).json({ message: "Usuário cadastrado com sucesso!", data: userData });
+      } else {
+        res.status(400).json({ error: 'Usuário não cadastrado.'});
+        return;
+      }       
+    }
+  });
+});
+
+module.exports = router;
