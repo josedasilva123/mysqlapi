14c1df076bbacce64d396a4f6e4911edda3ed461
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-09-16 15:13:55 -0300

    moving files

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import BookRoutes from "./routes/books.routes";
-import UserRoutes from "./routes/user.routes";
+import BookRoutes from "./routes/Books/books.routes";
+import UserRoutes from "./routes/User/user.routes";
 
 export const app = express();
 
